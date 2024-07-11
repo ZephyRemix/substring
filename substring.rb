6828620cb023e84893d84229a6e4f0539b571506
ZephyRemix <hunyaochong@gmail.com>
2024-07-11 10:37:16 +0800

    Refactor code
    cleaned up variables names, and use each_with_object
    function instead, which provides an implicit return

--- substring.rb
+++ substring.rb
@@ -1,11 +1,10 @@
-def substrings(str, dict)
-  word_arr = str.split(" ")
+def substrings(sentence, dictionary)
+  words = sentence.split
 
-  dict.reduce(Hash.new(0)) do |word_count_hash, word| 
-    for str_component in word_arr
-      word_count_hash[word] += 1 if str_component.downcase.include?(word)
+  dictionary.each_with_object(Hash.new(0)) do |dict_word, word_count|
+    words.each do |word|
+      word_count[dict_word] += 1 if word.downcase.include?(dict_word)
     end
-    word_count_hash
   end
 end
 
